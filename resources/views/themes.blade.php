de4b46037307448d9635d528b608e8b9807f002c
pututwibowo <putut.info@gmail.com>
2024-08-09 20:52:07 +0700

    membuat pagination di page tema

--- resources/views/themes.blade.php
+++ resources/views/themes.blade.php
@@ -94,7 +94,7 @@
         </ol>
     </nav>
 
-    <div class="grid grid-cols-4 gap-4 pt-10 px-32">
+    <div class="grid grid-cols-4 gap-4 mb-10 pt-20 px-32">
         @foreach ( $dataDB as $data)            
             <x-card 
                 :img="$data['img']"
@@ -103,4 +103,34 @@
             />
         @endforeach
     </div>
+
+    <div class="flex flex-col items-center mb-5">
+        <!-- Help text -->
+        <span class="text-sm text-gray-600 ">
+            Showing <span class="font-semibold text-gray-900">1</span> to <span class="font-semibold text-gray-900">10</span> of <span class="font-semibold text-gray-900">100</span> Entries
+        </span>
+
+        <div class="inline-flex mt-2 xs:mt-0">
+            <!-- Buttons -->
+            <button class="flex items-center justify-center px-3 h-8 text-sm font-medium rounded-s text-white bg-gray-800 hover:bg-gray-900">
+                <svg class="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+                </svg>
+
+                Prev
+            </button>
+
+            <button class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900">
+                Next
+
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </button>
+        </div>
+    </div>
+
+    <footer class="py-20">
+
+    </footer>
 </x-home-layout>
